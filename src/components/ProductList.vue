dd7f2f21988e9003328ada3b20ce1ae98f3728fd
xieyuecong <xieyuecong@0easy.com>
2019-08-07 11:32:10 +0800

    初始化

---
+++ src/components/ProductList.vue
@@ -0,0 +1,67 @@
+<template>
+  <ul>
+    <li
+      v-for="product in products"
+      :key="product.id">
+      <p>型号：{{ product.title }}&nbsp;&nbsp;&nbsp;售价：{{ product.price }}</p>
+      库存&nbsp;{{ product.inventory }}&nbsp;
+      <input type="number" style="width:40px;" min="0" :max="product.inventory" v-model="numbers[product.id]"/>&nbsp;&nbsp;&nbsp;
+      <button
+        :disabled="!product.inventory"
+        @click="addProductToCart(product)">
+        加入购物车
+      </button>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      numbers: {}
+    }
+  },
+  watch: {
+    products: {
+      handler (results) {
+        results.forEach(product => {
+          if (this.numbers[product.id] === undefined) {
+            this.$set(this.numbers, product.id, product.inventory > 0 ? 1 : 0);
+          }
+        })
+      },
+      immediate: true
+    }
+  },
+  computed: mapState({
+    products: state => state.products.all,
+  }),
+  // computed: {
+  //   products(){
+  //     return this.$store.state.products.all
+  //   }
+  // },
+  // methods: mapActions('cart', [
+  //   'addProductToCart'
+  // ]),
+  methods: {
+    addProductToCart(product){
+      if (this.numbers[product.id] == 0 ) {
+        alert("商品数量为空");
+        return;
+      } else if (this.numbers[product.id] > product.inventory) {
+        alert("商品数量超过库存");
+        return; 
+      }
+      this.$store.dispatch('cart/addProductToCart', {product, number: this.numbers[product.id]});
+      this.numbers[product.id] = product.inventory > 0 ? 1 : 0;
+    }
+  },
+  created () {
+    this.$store.dispatch('products/getAllProducts')
+  }
+}
+</script>
---
+++ src/store/modules/products.js
@@ -0,0 +1,46 @@
+import shop from '../../api/shop'
+import {PRODUCTS} from '../mutation-types'
+
+// initial state
+const state = {
+  all: []
+}
+
+// getters
+const getters = {}
+
+// actions
+const actions = {
+  getAllProducts ({ commit }) {
+    shop.getProducts(products => {
+      commit(PRODUCTS.SET_PRODUCTS, products)
+    })
+  }
+}
+
+// mutations
+const mutations = {
+  [PRODUCTS.SET_PRODUCTS] (state, products) {
+    state.all = products
+  },
+
+  [PRODUCTS.DECREMENT_PRODUCT_INVENTORY] (state, { id, number }) {
+    const product = state.all.find(product => product.id === id)
+    product.inventory -= number
+  },
+  // Test:将购物车清空的商品还原到仓库中
+  [PRODUCTS.RESET_PRODUCTS] (state, {items} ) {
+    items.forEach(element => {
+      const product = state.all.find(product => product.id === element.id);
+      product.inventory += parseInt(element.quantity);
+    });
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
